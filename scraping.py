cd43ceea7920132f02e76292be46df64dd4ef14b
aravinthvp <34703046+aravinthvp@users.noreply.github.com>
2018-08-23 07:39:10 +0530

    Add files via upload

---
+++ scraping.py
@@ -0,0 +1,17 @@
+import requests
+import bs4
+res =requests.get('https://aravinthvp.github.io')
+type(res)
+res.text()  # it is for page full source code
+
+
+#Beautifulsoup
+soup = bs4.BeautifulSoup(res.text,'lxml')
+type(soup)
+
+#it is for Specific needs
+need = soup.select('title')
+need
+
+
+need [0].getText()
